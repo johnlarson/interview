aa6a4e723130af07fb21b7f5508d1a44763cdd2e
John Larson <john.robert.larson@gmail.com>
2017-07-02 19:40:32 -0600

    BoardSetup._parse_fen() with stubs

--- boardsetup.py
+++ boardsetup.py
@@ -6,7 +6,12 @@ class BoardSetup:
         self._parse_fen(fen_str)
 
     def _parse_fen(self, fen_list):
-        ...
+        self._parse_board(fen_list[0])
+        self._parse_turn(fen_list[1])
+        self._parse_castling(fen_list[2])
+        self._parse_en_passant(fen_list[3])
+        self._parse_halfmove_clock(fen_list[4])
+        self._parse_fullmove_number(fen_list[5])
 
     def _parse_board(self, board_str):
         ...
