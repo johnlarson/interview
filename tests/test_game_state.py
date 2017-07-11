50c25430b1b1b382c72cd28e5befe4c84321d0a9
John Larson <john.robert.larson@gmail.com>
2017-07-11 12:30:03 -0600

    unit test stubs

---
+++ tests/test_game_state.py
@@ -0,0 +1,316 @@
+from unittest import TestCase
+
+
+class InitTests(TestCase):
+    """Unit tests for GameState.__init__()"""
+
+    def test_valid_input(self):
+        """Should correctly parse a valid fen file."""
+        ...
+
+    def test_no_fen_file_path(self):
+        """
+        If no fen file is provided, should raise NoFENFileProvidedError.
+        """
+        ...
+
+    def test_fen_file_does_not_exist(self):
+        """
+        If no fen file exists at provided path, should raise
+        FENFileDoesNotExistError.
+        """
+        ...
+
+    def test_invalid_fen_file(self):
+        """
+        If no fen file exists at the provided file path, should raise
+        InvalidFENFileError.
+        """
+        ...
+
+    def test_parse_positions(self):
+        """Should correctly parse positions of pieces on board."""
+
+    def test_whites_turn(self):
+        """
+        Tests the parsing of the turn field in the FEN file when
+        it is white player's turn.
+        """
+        ...
+
+    def test_blacks_turn(self):
+        """
+        Tests the parsing of the turn field in the FEN file when
+        it is black player's turn.
+        """
+        ...
+
+    def test_castling_all(self):
+        """
+        Tests the parsing of castling when all kings and queens can be
+        castled.
+        """
+        ...
+
+    def test_castling_some(self):
+        """
+        Tests the parsing of castling when some kings and queens can be
+        castled.
+        """
+        ...
+
+    def test_castling_none(self):
+        """
+        Tests the parsing of castling when no kings or queens can be
+        castled.
+        """
+        ...
+
+    def test_en_passant(self):
+        """
+        Should correctly parse when FEN file contains an en passant
+        position.
+        """
+        ...
+
+    def test_no_en_passant(self):
+        """Should correctly parse when en passant field is '-'."""
+        ...
+
+    def test_halfmove_clock(self):
+        """Should correctly parse the halfmove clock."""
+        ...
+
+    def test_halfmove_clock_multi_digit(self):
+        """Should correctly parse multi-digit halfmove clock."""
+        ...
+
+    def test_fullmove_number(self):
+        """Should correctly parse the fullmove number."""
+        ...
+
+    def test_fullmove_number_multi_digit(self):
+        """Should correctly parse multi-digit fullmove number."""
+        ...
+
+
+class FenTests(TestCase):
+    """Tests the `fen` property method."""
+
+    def test_correctly_display(self):
+        """
+        Should return a correct fen-formatted description of the current
+        game state.
+        """
+        ...
+
+    def test_correctly_display_board(self):
+        """Board state should be correct in returned fen string."""
+        ...
+
+    def test_whites_turn(self):
+        """
+        Should return correct description of turn when it is white's
+        turn.
+        """
+        ...
+
+    def test_blacks_turn(self):
+        """
+        Should return correct description of turn when it is black's
+        turn.
+        """
+        ...
+
+    def test_castling_all(self):
+        """
+        Should return correct description of castling when all kings and
+        queens are castleable.
+        """
+        ...
+
+    def test_castling_some(self):
+        """
+        Should return correct description of castling when some kings
+        and queens are castleable.
+        """
+        ...
+
+    def test_castling_none(self):
+        """
+        Should return correct description of castling when no kings or
+        queens are castleable.
+        """
+        ...
+
+    def test_en_passant(self):
+        """Should return correct description of en passant square."""
+        ...
+
+    def test_no_en_passant(self):
+        """
+        Should give correct description of en passant when there is no
+        current en passant square.
+        """
+        ...
+
+    def test_halfmove_clock(self):
+        """Should correctly display the halfmove clock."""
+        ...
+
+    def test_halfmove_clock_multi_digit(self):
+        """Should correctly display a multi-digit halfmove clock."""
+        ...
+
+    def test_fullmove_number(self):
+        """Should correctly display the fullmove number."""
+        ...
+
+    def test_fullmove_number_multi_digit(self):
+        """Should correctly display a multi-digit fullmove number."""
+        ...
+
+
+class BoardTextTests(TestCase):
+    """Tests `board_text` property method of GameState."""
+
+    def test_correctly_display(self):
+        """
+        Should correctly display the board based on the state of the
+        GameState object.
+        """
+        ...
+
+
+class TakeTurnTests(TestCase):
+    """Tests GameState.take_turn()"""
+
+    def test_makes_suggested_move(self):
+        """Should make move suggested by api."""
+        ...
+
+    def test_correctly_handles_capturing(self):
+        """
+        When one piece lands on another, the other should be removed
+        from the board, and moving piece should replace it.
+        """
+        ...
+
+    def test_white_turn_to_black(self):
+        """
+        When white makes a move, the turn tracker should be set to
+        black's turn.
+        """
+        ...
+
+    def test_black_turn_to_white(self):
+        """
+        When black makes a move, the turn tracker should be set to
+        white's turn.
+        """
+        ...
+
+    def test_castling_move_white_king(self):
+        """Moving white king should disable all white castling."""
+        ...
+
+    def test_castling_move_white_king_side_rook(self):
+        """
+        Moving white king-side rook should disable it for castling.
+        """
+        ...
+
+    def test_castling_move_white_queen_side_rook(self):
+        """
+        Moving white queen-side rook should disable it for castling.
+        """
+        ...
+
+    def test_castling_move_black_king(self):
+        """Moving black king should disable all black castling."""
+        ...
+
+    def test_castling_move_black_king_side_rook(self):
+        """
+        Moving black king-side rook should disable it for castling.
+        """
+        ...
+
+    def test_castling_move_black_queen_side_rook(self):
+        """
+        Moving black queen-side rook should disable it for castling.
+        """
+        ...
+
+    def test_castling_move_other_piece(self):
+        """
+        Moving any piece other than a king or rook should not disable
+        castling.
+        """
+        ...
+
+    def test_en_passant_pawn_moves_two_squares(self):
+        """
+        When a pawn moves two spaces, the square behind it should be
+        set as the en passant square.
+        """
+        ...
+
+    def test_en_passant_only_lasts_one_turn(self):
+        """
+        Even if there was previously an en passant square, if no pawn
+        moved two spaces this turn, en passant should be set to None.
+        """
+        ...
+
+    def test_en_passant_non_pawn_moves(self):
+        """When a non-pawn piece moves, en passant should be None."""
+        ...
+
+    def test_en_passant_non_pawn_moves_two_squares(self):
+        """
+        Even if a non-pawn moves two spaces, en passant should be None.
+        """
+        ...
+
+    def test_en_passant_pawn_moves_one_square(self):
+        """
+        If pawn moves but it does not move two squares, en passant
+        should be None.
+        """
+        ...
+
+    def test_halfmove_clock_increments_after_player_makes_move(self):
+        """
+        When a player makes a reversible move, the halfmove clock should
+        be incremented.
+        """
+        ...
+
+    def test_halfmove_clock_resets_on_capture(self):
+        """
+        When a player captures a piece, the halfmove clock should be
+        reset to 0.
+        """
+        ...
+
+    def test_halfmove_clock_resets_on_pawn_move(self):
+        """
+        When a player moves a pawn, the halfmove clock should be reset
+        to 0.
+        """
+        ...
+
+    def test_halfmove_clock_resets_on_castling(self):
+        """
+        When a player makes a castling move, the halfclock should be
+        reset, as this is an irreversible move (see
+        http://www.open-chess.org/viewtopic.php?f=3&t=2209).
+        """
+        ...
+
+    def test_fullmove_number_increments_when_black_takes_a_turn(self):
+        ...
+
+    def test_fullmove_number_does_not_increment_when_white_moves(self):
+        ...
