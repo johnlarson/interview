1a7e194161ad6ff554743410b468dbb4e72021c5
John Larson <john.robert.larson@gmail.com>
2017-07-12 21:49:10 -0600

    functional test stubs

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -4,19 +4,64 @@ from unittest import TestCase
 class Task1Tests(TestCase):
     """Functional tests for Task 1."""
 
-    def test_(self):
+    def test_valid_input(self):
+        """If input is valid, it should correctly display the board."""
+        ...
+
+    def test_no_fen_file_provided(self):
+        """If the user does not provide a fen file, show usage."""
+        ...
+
+    def test_invalid_file_path(self):
+        """If file path is invalid, display 'File not found.'"""
+        ...
+
+    def test_invalid_fen_file(self):
+        """If fen file is invalid, display 'Invalid FEN file.'"""
         ...
 
 
 class Task2Tests(TestCase):
     """Functional tests for Task 2."""
 
-    def test_(self):
+    def test_valid_input(self):
+        """
+        If input is valid, it should make the move directed by the api
+        and display the resulting fen-formatted text.
+        """
+        ...
+
+    def test_no_fen_file_provided(self):
+        """If the user does not provide a fen file, show usage."""
+        ...
+
+    def test_invalid_file_path(self):
+        """If file path is invalid, display 'File not found.'"""
+        ...
+
+    def test_invalid_fen_file(self):
+        """If fen file is invalid, display 'Invalid FEN file.'"""
         ...
 
 
 class StretchTaskTests(TestCase):
     """Functional tests for stretch task."""
 
-    def test_(self):
+    def test_valid_input(self):
+        """
+        If input is valid, it should make the move directed by the api
+        and display the resulting board and fen-formatted game state.
+        """
+        ...
+
+    def test_no_fen_file_provided(self):
+        """If the user does not provide a fen file, show usage."""
+        ...
+
+    def test_invalid_file_path(self):
+        """If file path is invalid, display 'File not found.'"""
+        ...
+
+    def test_invalid_fen_file(self):
+        """If fen file is invalid, display 'Invalid FEN file.'"""
         ...
