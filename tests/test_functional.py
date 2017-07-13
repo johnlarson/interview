a62900ed8d8cfe75fce40b50558d5922c18b541a
John Larson <john.robert.larson@gmail.com>
2017-07-12 21:24:05 -0600

    started functional tests

---
+++ tests/test_functional.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+
+class Task1Tests(TestCase):
+    """Functional tests for Task 1."""
+
+    def test_(self):
+        ...
+
+
+class Task2Tests(TestCase):
+    """Functional tests for Task 2."""
+
+    def test_(self):
+        ...
+
+
+class StretchTaskTests(TestCase):
+    """Functional tests for stretch task."""
+
+    def test_(self):
+        ...
